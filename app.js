7022652cc14f95680c9b13bc7912a183b37826a4
camilozv21 <camilozv21@gmail.com>
2023-08-09 22:57:07 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+let session = require('express-session');
+const methodOverride = require('method-override');
+const PORT = process.env.PORT || 4001;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+app.use(methodOverride("_method"));
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+app.set('views', [
+    path.join(__dirname, './views/main'),
+    path.join(__dirname, './views/user'),
+    path.join(__dirname, './views/feed')
+]);
+
+// Routes const userRouter = require('./routes/userRoutes.js');
+
+// Use routes // app.use('/user', userRouter);
+
+app.use((req, res, next) => {
+    res.status(404).render("./not-found")
+});
+
+app.listen(PORT, () => {console.log(`Servidor escuchando puerto http://localhost:${PORT} 🚀`)});
